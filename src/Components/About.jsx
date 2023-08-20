511f5f15817f86eb12216db464ca2d1f7705595e
Adedamola7 <69017300+Adedamola7@users.noreply.github.com>
2023-08-20 05:31:58 -0700

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "A picture of a cabin in the woods.";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a Computer Science student studying at Federal University Oye-Ekiti Ekiti State. I enjoy creating solutions that makes life easier fro human.";
+  "I'm a Computer Science student studying at Federal University Oye-Ekiti Ekiti State. I enjoy creating solutions that makes life easier for human.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my software development experience I continually look for new and better ways to make innovations.";
 
 const About = () => {
   return (
